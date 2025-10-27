639b47f2ffa21818868ea86eeebc5254a0e4aeca
mad-swami <manuel.dtp.f@gmail.com>
2025-10-27 01:55:32 -0400

    Made updates to the main script and made finish episode
    Moved constants to the top of the main script for use in further functions. Specifically the actions the agent can take and the goal reward, wall penalty, and step penalty.
    
    Made finish episode function for working the agent through an episode of the maze.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+val actions = arrayOf(
+    Pair(-1, 0), // down
+    Pair(1, 0), // up
+    Pair(0, 1), // right
+    Pair(0, -1) // left
+)
+
+const val goalReward = 100
+const val wallPenalty = -10
+const val stepPenalty = -1
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -20,12 +31,46 @@ fun main() {
 
     // create a Q Learning Maze object
     val maze = QLearningMaze(mazeLayout, startingPoint, endingPoint)
+}
 
-    // actions the agent can take: down, up, right, left
-    val actions = arrayOf(
-        Pair(-1, 0), // down
-        Pair(1, 0), // up
-        Pair(0, 1), // right
-        Pair(0, -1)) // left
+fun finishEpisode(agent: QLearningAgent, maze: QLearningMaze, currentEpisode: Int, train: Boolean = true): Array<Any> {
+    var currentState = maze.startPos
+    var isDone = false
+    var episodeReward = 0.0
+    var episodeStep = 0
+    val path = mutableListOf(currentState)
 
+    while (!isDone) {
+        val action = agent.getAction(currentState, currentEpisode)
+
+        var nextState = Pair(currentState.first + actions[action].first, currentState.second + actions[action].second)
+        var reward: Double
+
+        if ((nextState.first < 0) || (nextState.first >= maze.mazeHeight)
+            || (nextState.second < 0) || (nextState.second >= maze.mazeWidth)
+            || (maze.mazeLayout[nextState.first][nextState.second] == 1)
+        ) {
+
+            reward = wallPenalty.toDouble()
+            nextState = currentState
+
+        } else if (nextState == maze.endPos) {
+            path.add(currentState)
+            reward = goalReward.toDouble()
+            isDone = true
+        } else {
+            path.add(currentState)
+            reward = stepPenalty.toDouble()
+        }
+
+        episodeReward += reward
+        episodeStep += 1
+
+        if (train) {
+            agent.updateQTable(currentState, action, nextState, reward)
+        }
+
+        currentState = nextState
+    }
+    return arrayOf(episodeReward, episodeStep, path)
 }
