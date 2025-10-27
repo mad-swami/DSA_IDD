c08953f8ba503efbeb5c0603d0d96342173df50d
mad-swami <manuel.dtp.f@gmail.com>
2025-10-26 21:47:34 -0400

    Started main script for our qlearning procedure
    Created a sample maze layout and set a starting and ending point for the maze. Then create a qlearningmaze object for the future. Finally assign the actions the agent will be able to take to traverse the maze.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,29 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    // initialize the maze layout that we want to use for our tests
+    // 1s represent walls and 0s represent empty spaces
+    val mazeLayout = arrayOf(
+        intArrayOf(0, 1, 0, 0, 0),
+        intArrayOf(0, 1, 1, 1, 0),
+        intArrayOf(0, 0, 0, 1, 0),
+        intArrayOf(1, 1, 0, 1, 1),
+        intArrayOf(0, 0, 0, 0, 0)
+    )
+
+    // set the starting and ending points of the maze
+    val startingPoint = Pair(0, 0)
+    val endingPoint = Pair(4, 4)
+
+    // create a Q Learning Maze object
+    val maze = QLearningMaze(mazeLayout, startingPoint, endingPoint)
+
+    // actions the agent can take: down, up, right, left
+    val actions = arrayOf(
+        Pair(-1, 0), // down
+        Pair(1, 0), // up
+        Pair(0, 1), // right
+        Pair(0, -1)) // left
+
 }
