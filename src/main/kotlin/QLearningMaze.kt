8e40dd6eae1e4fd7d2aae49c51ed1e734dce40d8
mad-swami <manuel.dtp.f@gmail.com>
2025-10-27 01:21:35 -0400

    Fleshed out documentation for qlearningmaze class
    Added definitions for the class and defined parameters and properties for the class.

--- src/main/kotlin/QLearningMaze.kt
+++ src/main/kotlin/QLearningMaze.kt
@@ -3,14 +3,17 @@ package org.example
 /**
  * Class to store values of the specific maze type to be used in this QLearning Project
  *
- * @param mazeDiagram a 2d array of
+ * Stores a maze in the form of given [mazeLayout], a 2d array of 1s and 0s. Start and end positions of the maze are given
+ * and stored within [startPos] and [endPos]. The shape of the maze is found and kept within [mazeHeight] and
+ * [mazeWidth]. The function showMaze is used to display the maze in a visual.
+ *
+ * @param mazeLayout a 2d array of ints of 1s and 0s representing empty walls and spaces respectively
+ * @param startPos a pair of ints representing the start position in the maze
+ * @param endPos a pair of ints representing the end position in the maze
+ * @property mazeHeight
+ * @property mazeWidth
  */
-class QLearningMaze(mazeDiagram: Array<IntArray>, startPoint: Pair<Int, Int>, endPoint: Pair<Int, Int>): Maze {
-    override val mazeLayout = mazeDiagram
-
-    override val startPos = startPoint
-    override val endPos = endPoint
-
+class QLearningMaze(override val mazeLayout: Array<IntArray>, override val startPos: Pair<Int, Int>, override val endPos: Pair<Int, Int>): Maze {
     override val mazeHeight = mazeLayout.size
     override val mazeWidth = mazeLayout[0].size
 
