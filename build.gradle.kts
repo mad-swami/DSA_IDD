3b3ddbdac5b51d213908288c149b746b518a502b
mad-swami <manuel.dtp.f@gmail.com>
2025-10-27 14:31:54 -0400

    Added compose to build
    Added compose so i can visualize the agent moving through the maze

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
+    id("org.jetbrains.compose") version "1.9.1"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs)
 }
 
 tasks.test {
@@ -18,4 +23,10 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(24)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
